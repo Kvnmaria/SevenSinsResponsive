2ca2dda3291fbc8936b8969b8b10de1f98c08573
Kvnmaria <65904891+Kvnmaria@users.noreply.github.com>
2021-06-23 19:45:33 +0530

    commit on 'master'

---
+++ script.js
@@ -0,0 +1,24 @@
+const btnGrid = document.querySelector('.btn-grid');
+const btnFlex = document.querySelector('.btn-flex');
+const hidden = document.querySelector('.hidden');
+const hiddenGrid = document.querySelector('.hidden-1');
+
+
+btnGrid.addEventListener('click', function () {
+
+
+    hidden.style.display = 'none';
+    hiddenGrid.classList.remove('hidden-1');
+
+
+})
+
+btnFlex.addEventListener('click', function () {
+
+
+
+    hiddenGrid.classList.add('hidden-1');
+    hidden.style.display = 'contents';
+
+
+})
